84a0e2eb70793a8bd4e26127227f8ab7f1ce606d
shane <hunghsuyong114shane@gmail.com>
2020-02-02 22:35:44 +0800

    add argparse in serial writing and reading script

--- lora/serial_read.py
+++ lora/serial_read.py
@@ -1,10 +1,23 @@
 #!/usr/bin/env python3
 import time
 import serial
+import argparse
+
+parser = argparse.ArgumentParser(description="setting for serial communication!")
+parser.add_argument("--port", required=True, type=str,
+        help="this is the serial port chosen", 
+        choices=["/dev/ttyS0", "/dev/ttyAM0", "/dev/ttyUSB0"])
+parser.add_argument("--baud", required=True, type=int,
+        help="this is the baudrate for UART transmission",
+        choices=[1200, 4800, 9600, 19200, 38400, 115200])
+
+args = parser.parse_args()
+port = args.port 
+baud = args.baud
 
 ser = serial.Serial(
-        port='/dev/ttyUSB0',
-        baudrate = 9600,
+        port = port,
+        baudrate = baud,
         parity=serial.PARITY_NONE,
         stopbits=serial.STOPBITS_ONE,
         bytesize=serial.EIGHTBITS,
--- lora/serial_write.py
+++ lora/serial_write.py
@@ -2,9 +2,23 @@
 import time
 import serial
 import datetime
+import argparse
+
+parser = argparse.ArgumentParser(description="setting for serial communication!")
+parser.add_argument("--port", required=True, type=str, 
+        help="this is the serial port chosen", 
+        choices=["/dev/ttyS0", "/dev/ttyAM0", "/dev/ttyUSB0"])
+parser.add_argument("--baud", required=True, type=int, 
+        help="this is the baudrate for UART transmission",
+        choices=[1200, 4800, 9600, 19200, 38400, 115200])
+
+args = parser.parse_args()
+port = args.port 
+baud = args.baud
+
 ser = serial.Serial(
-        port='/dev/ttyS0', #Replace ttyS0 with ttyAM0 for Pi1,Pi2,Pi0
-        baudrate = 9600,
+        port = port,
+        baudrate = baud,
         parity=serial.PARITY_NONE,
         stopbits=serial.STOPBITS_ONE,
         bytesize=serial.EIGHTBITS,
